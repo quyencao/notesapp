e2f31a72172772d84f0490c8eebd6b87c449f3dc
quyencao <boy20195@gmail.com>
2017-03-09 16:07:37 +0700

    Make ajax call to signup.php

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // All ajax call point to signup.php, activate.php
+$("#signupForm").submit(function (event) {
+    // prevent default php processing
+    event.preventDefault();
+    // collect all user input
+    var datatopost = $(this).serializeArray();
+    // ajax call
+    $.ajax({
+        url: "signup.php",
+        type: "POST",
+        data: datatopost,
+        success: function (data) {
+            if(data) {
+                $("#signupmessage").html(data);
+            }
+        },
+        error: function (data) {
+            if(data) {
+                $("#signupmessage").html("<div class='alert alert-danger'>There was an error. Try again later</div>")
+            }
+        }
+    });
+});
